451f39f6eef95dce2cd78f8b2f4eecad08c08050
kyllopardiun <mansueli@engineer.com>
2014-10-10 16:13:42 -0600

    Project 1 ECE422 Ualberta RC1

--- jni/sort.c
+++ jni/sort.c
@@ -13,24 +13,15 @@ void insertSort(jint*, jsize);
  * Signature: ([I)[I
  */
 JNIEXPORT jintArray JNICALL Java_ece422_project1_variants_InsertionSort_sort
-    (JNIEnv *env, jobject obj, jintArray arr){
-    jsize arrLength = env->GetArrayLength(arr);
-    jintArray arrSorted = env->NewIntArray(arrLength); 
-    jint *arrOut = NULL;
-    arrOut = env->GetIntArrayElements(arr, 0);
-//    for(jsize x = 0; x < arrLength; x++){
-//        for(jsize y = 0; y < arrLength - 1; y++){
-//                        if(arrOut[y] > arrOut[y+1]){
-//				jsize temp = arrOut[y+1];
-//				arrOut[y+1] = arrOut[y];
-//				arrOut[y] = temp;
-//			}
-//		}
-//	}
-    insertSort(arrOut, arrLength);
+(JNIEnv *env, jobject obj, jintArray array) {
+    jsize arrayLength = env->GetArrayLength(array);
+    jintArray arrSorted = env->NewIntArray(arrayLength);
+    jint *outputArray = NULL;
+    outputArray = env->GetIntArrayElements(array, 0);
+    insertSort(outputArray, arrayLength);
     printf("\nMemory accesses occured in InsertionSort: %d\n", memoryAccess);
-    env->SetIntArrayRegion(arrSorted, 0, arrLength, arrOut);
-    env->ReleaseIntArrayElements(arr, arrOut, 0);
+    env->SetIntArrayRegion(arrSorted, 0, arrayLength, outputArray);
+    env->ReleaseIntArrayElements(array, outputArray, 0);
     return arrSorted;
 }
 
@@ -40,49 +31,23 @@ JNIEXPORT jintArray JNICALL Java_ece422_project1_variants_InsertionSort_sort
  * @param p jint Array to be sorted
  * @param size size of the array that will be sorted
  */
-void insertSort(jint *p, jsize size){
+void insertSort(jint *p, jsize size) {
     int key, i;
-    if (size>1){
-        insertSort(p, size-1);
+    if (size > 1) {
+        insertSort(p, size - 1);
         memoryAccess++;
         key = p[size];
         memoryAccess++;
-        i = size -1;
+        i = size - 1;
         memoryAccess++;
-        while(i>0 && p[i]>key){
+        while (i > 0 && p[i] > key) {
             memoryAccess++;
-            p[i+1] = p[i];
+            p[i + 1] = p[i];
             memoryAccess++;
             i--;
             memoryAccess++;
         }
-        p[i+1]=key;
-        memoryAccess++;
-    }
-}
-/**
- * Based on the description on the site below:
- * Feofiloff, Paulo. Available in: <http://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/insert.html>
- * @param p jint Array to be sorted
- * @param size size of the array that will be sorted
- */
-void insertionSort(jint *p, int size){
-    int key, i;
-    if (size>1){
-        insertionSort(p, size-1);
-        memoryAccess++;
-        key = p[size];
-        memoryAccess++;
-        i = size -1;
-        memoryAccess++;
-        while(i>0 && p[i]>key){
-            memoryAccess++;
-            p[i+1] = p[i];
-            memoryAccess++;
-            i--;
-            memoryAccess++;
-        }
-        p[i+1]=key;
+        p[i + 1] = key;
         memoryAccess++;
     }
 }
